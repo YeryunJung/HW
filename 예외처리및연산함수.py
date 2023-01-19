4fd96b9a5f1ec157d5322b937a8625e0bb57d569
Yeryun Jung <103527404+YeryunJung@users.noreply.github.com>
2023-01-19 14:28:47 +0900

    Create 예외처리및연산함수.py

---
+++ 예외처리및연산함수.py
@@ -0,0 +1,26 @@
+import calc
+
+print(calc.add(2, 3)) # 5
+print(calc.sub(2, 3)) # -1
+print(calc.mul(2, 3)) # 6
+print(calc.div(2, 3)) # 0.6666666666666666
+
+print(calc.div(2, 0)) # 0으로 나눌 수 없습니다.
+
+######################################################
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        print('0으로는 나눌 수 없습니다.')
+        return
+    else: 
+        return a / b
