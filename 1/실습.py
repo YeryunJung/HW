6ec6f006dc03e605d408000da91013ad249533ea
YeryunJung <2bites@naver.com>
2023-01-23 00:32:01 +0900

    update 1-5

--- 1/실습.py
+++ 1/실습.py
@@ -1,23 +1,23 @@
-# # 1
-# data = "\"c:\\python_project\\test\""
-# print(data)
-# # 원하는 출력문
-# # "c:\python_project\test"
+# 1
+data = "\"c:\\python_project\\test\""
+print(data)
+# 원하는 출력문
+# "c:\python_project\test"
 
-# # 2
-# total_count = int(input('게시글의 총 갯수를 입력하세요 :'))
-# one_page_count = int(input('한 페이지에 필요한 게시글 수를 입력하세요 : '))
+# 2
+total_count = int(input('게시글의 총 갯수를 입력하세요 :'))
+one_page_count = int(input('한 페이지에 필요한 게시글 수를 입력하세요 : '))
 
-# page = int(total_count/one_page_count)
-# print(page)
+page = int(total_count/one_page_count)
+print(page)
 
-# # 3
-# person1 = input("첫 번째 사람의 이름을 입력하시오: ")
-# person2 = input("두 번째 사람의 이름을 입력하시오: ")
+# 3
+person1 = input("첫 번째 사람의 이름을 입력하시오: ")
+person2 = input("두 번째 사람의 이름을 입력하시오: ")
 
 
-# # print(f"{person1}\n\n{person2}")
-# print(person1, person2, sep="\n\n\n")
+# print(f"{person1}\n\n{person2}")
+print(person1, person2, sep="\n\n\n")
 
 # 4
 multiple = []
@@ -37,4 +37,15 @@ multiple = sum(multiple)
 
 print(multiple)
 
+# 5
+m = 5 #세로
+n = 4 #가로
 
+# ****
+# ****
+# ****
+# ****
+# ****
+
+for i in range(5):
+  print('****')
