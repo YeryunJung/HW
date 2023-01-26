4aeb2a2a72f90de7a659f19885db617ee277b8a4
Yeryun Jung <103527404+YeryunJung@users.noreply.github.com>
2023-01-26 13:35:35 +0900

    Update 실습.py

--- 6/실습.py
+++ 6/실습.py
@@ -14,6 +14,26 @@ def de_identify(identification_number):
 print(de_identify('970103-1234567'))
 print(de_identify('861123-2345678'))
 
+################################################################################
+#solving
+
+def de_identify(identification_number):
+    identification_number = identification_number.replace('-','')
+    # 문자열의 슬라이싱을 이용한 방법
+    return identification_number[:-7] + '****'
+
+print(de_identify('970103-1234567'))
+
+def de_identify(identification_number, slicing_num):
+    # '-'룰 ''으로 대체
+    identification_number = identification_number.replace('-','')
+    str_list = list(identification_number)
+    str_list[-slicing_num] = '****'
+    print(str_list)
+    return ''.join(str_list)
+    
+print(de_identify('970103-1234567'))
+
 # 2
 grain_lst = [('고구마',3000), ('감자',2000), ('옥수수',4500),('토란',1300)]
 
