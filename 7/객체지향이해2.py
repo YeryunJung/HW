13033401e326eab76917d67581f02ab050462c11
YeryunJung <2bites@naver.com>
2023-01-30 17:11:48 +0900

    update 객체지향이해2

--- 7/객체지향이해2.py
+++ 7/객체지향이해2.py
@@ -14,6 +14,47 @@ def collatz(num):
         
     return count
 
+##################################################
+# solving1
+
+def collatz(num):
+    for i in range(500):
+        if num % 2 == 0:
+            num = num / 2
+        else: # 홀수
+            num = (num * 3) + 1
+
+        if num == 1:
+            return i + 1 # 인덱스가 0부터 시작했으니까
+
+    return -1
+
+# solving2
+
+def collatz(num):
+    if num == 1:
+        return 0
+
+    ans = 0
+
+    if num % 2 == 0:
+        num //= 2
+        ans = collatz(num)
+        if ans != -1:
+            ans += 1
+
+    elif num % 2 == 1:
+        num = num * 3 + 1
+        ans = collatz(num)
+        if ans != -1:
+            ans += 1
+
+    if ans > 500:
+        return -1
+
+    return ans
+
+
 print(collatz(6)) #=> 8
 print(collatz(16)) #=> 4
 print(collatz(27)) #=> 111
